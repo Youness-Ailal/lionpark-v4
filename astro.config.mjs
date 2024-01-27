a07b731fe4b7d1fa09b34baccf4ac68b75f1fa6e
Youness-Ailal <itsyounessaylal@gmail.com>
2024-01-27 17:58:14 +0100

    v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), react()],
+  integrations: [icon(), react(), sitemap()],
+  site: "https://lionpark.ailal.dev",
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr"],
--- src/i18n/utils.js
+++
@@ -1,7 +0,0 @@
-export const LANGUAGES = {
-  en: "English",
-  fr: "Français",
-  es: "Español",
-};
-
-export const DEFAULT_LANG = "en";
